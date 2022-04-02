06d56a9080d4ed052b7febbd371c4438696c6930
snowyru <97807830+snowyru@users.noreply.github.com>
2022-04-02 18:41:27 +0200

    removed links from deleted files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LayoutRoute from "./LayoutRoute.js";
-import MainScreen from './MainScreen.js';
-import AboutScreen from "./AboutScreen.js";
-import PricingScreen from "./PricingScreen.js";
-import ProfileSettingsScreen from "./ProfileSettingsScreen.js";
 import BGvid from "./BGvid.js";
 
 function App() {
@@ -11,9 +7,6 @@ function App() {
         <BrowserRouter>
             <Switch>
                 <LayoutRoute path="/" component={BGvid} exact={true} />
-                <LayoutRoute path="/about" component={AboutScreen} exact={true} />
-                <LayoutRoute path="/pricing" component={PricingScreen} exact={true} />
-                <LayoutRoute path="/profile-settings" component={ProfileSettingsScreen} exact={true} />
             </Switch>
         </BrowserRouter>
     )
