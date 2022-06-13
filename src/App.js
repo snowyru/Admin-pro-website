f5acbab2fa2cae68a34f178f60533b52f82630b3
snowyru <97807830+snowyru@users.noreply.github.com>
2022-06-13 19:07:52 +0200

    Updated to MDB

--- src/App.js
+++ src/App.js
@@ -1,210 +1,35 @@
-import React, { Component } from 'react';
-import {
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarNav,
-  MDBNavbarToggler,
-  MDBCollapse,
-  MDBNavItem,
-  MDBFooter,
-  MDBNavLink,
-  MDBTooltip,
-  MDBIcon
-} from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ReactComponent as Logo } from './assets/logo.svg';
-import Routes from './Routes';
+import React from 'react';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
 
-class App extends Component {
-  state = {
-    collapseID: ''
-  };
-
-  toggleCollapse = collapseID => () =>
-    this.setState(prevState => ({
-      collapseID: prevState.collapseID !== collapseID ? collapseID : ''
-    }));
-
-  closeCollapse = collID => () => {
-    const { collapseID } = this.state;
-    window.scrollTo(0, 0);
-    collapseID === collID && this.setState({ collapseID: '' });
-  };
-
-  render() {
-    const overlay = (
+function App() {
+  return (
+    <MDBContainer fluid>
       <div
-        id='sidenav-overlay'
-        style={{ backgroundColor: 'transparent' }}
-        onClick={this.toggleCollapse('mainNavbarCollapse')}
-      />
-    );
-
-    const { collapseID } = this.state;
-
-    return (
-      <Router>
-        <div className='flyout'>
-          <MDBNavbar color='indigo' dark expand='md' fixed='top' scrolling>
-            <MDBNavbarBrand href='/' className='py-0 font-weight-bold'>
-              <Logo style={{ height: '2.5rem', width: '2.5rem' }} />
-              <strong className='align-middle'>MDB React</strong>
-            </MDBNavbarBrand>
-            <MDBNavbarToggler
-              onClick={this.toggleCollapse('mainNavbarCollapse')}
-            />
-            <MDBCollapse id='mainNavbarCollapse' isOpen={collapseID} navbar>
-              <MDBNavbarNav right>
-                <MDBNavItem>
-                  <MDBNavLink
-                    exact
-                    to='/'
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                  >
-                    <strong>Home</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/css'
-                  >
-                    <strong>CSS</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/components'
-                  >
-                    <strong>Components</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/advanced'
-                  >
-                    <strong>Advanced</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/navigation'
-                  >
-                    <strong>Navigation</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/forms'
-                  >
-                    <strong>Forms</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/tables'
-                  >
-                    <strong>Tables</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/modals'
-                  >
-                    <strong>Modals</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink
-                    onClick={this.closeCollapse('mainNavbarCollapse')}
-                    to='/addons'
-                  >
-                    <strong>Addons</strong>
-                  </MDBNavLink>
-                </MDBNavItem>
-
-                
-
-                <MDBNavItem>
-                  <MDBTooltip
-                    placement='bottom'
-                    domElement
-                    style={{ display: 'block' }}
-                  >
-                    <a
-                      className='nav-link Ripple-parent'
-                      href='https://mdbootstrap.com/products/react-ui-kit/'
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      <strong>
-                        <MDBIcon far icon='gem' />
-                      </strong>
-                    </a>
-                    <span>PRO</span>
-                  </MDBTooltip>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBTooltip
-                    placement='bottom'
-                    domElement
-                    style={{ display: 'block' }}
-                  >
-                    <a
-                      className='nav-link Ripple-parent'
-                      href='https://mdbootstrap.com/docs/react/getting-started/download/'
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      <strong>
-                        <MDBIcon icon='download' />
-                      </strong>
-                    </a>
-                    <span>FREE</span>
-                  </MDBTooltip>
-                </MDBNavItem>
-                <MDBNavItem className='mr-2'>
-                  <MDBTooltip
-                    placement='bottom'
-                    domElement
-                    style={{ display: 'block' }}
-                  >
-                    <a
-                      className='nav-link Ripple-parent'
-                      href='https://mdbootstrap.com/support/cat/react/'
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      <strong>
-                        <MDBIcon icon='question-circle' />
-                      </strong>
-                    </a>
-                    <span>SUPPORT</span>
-                  </MDBTooltip>
-                </MDBNavItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
-          </MDBNavbar>
-          {collapseID && overlay}
-          <main style={{ marginTop: '4rem' }}>
-            <Routes />
-          </main>
-          <MDBFooter color='indigo'>
-            <p className='footer-copyright mb-0 py-3 text-center'>
-              &copy; {new Date().getFullYear()} Copyright:
-              <a href='https://www.MDBootstrap.com'> MDBootstrap.com </a>
-            </p>
-          </MDBFooter>
+        className='d-flex justify-content-center align-items-center'
+        style={{ height: '100vh' }}
+      >
+        <div className='text-center'>
+          <img
+            className='mb-4'
+            src='https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'
+            style={{ width: 250, height: 90 }}
+          />
+          <h5 className='mb-3'>
+            Thank you for using our product. We're glad you're with us.
+          </h5>
+          <p className='mb-3'>MDB Team</p>
+          <MDBBtn
+            tag='a'
+            href='https://mdbootstrap.com/docs/standard/getting-started/'
+            target='_blank'
+            role='button'
+          >
+            Start MDB tutorial
+          </MDBBtn>
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </MDBContainer>
+  );
 }
 
 export default App;
