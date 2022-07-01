3835d1e4f40f144d3a523ba4af82d1893492319f
snowyru <97807830+snowyru@users.noreply.github.com>
2022-07-01 21:17:29 +0200

    added framer

--- src/APPages/About.jsx
+++ src/APPages/About.jsx
@@ -1,22 +1,54 @@
 import React from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+import { motion } from "framer-motion";
+
+const icon = {
+  hidden: {
+    pathLength: 0,
+    fill: "rgba(255, 255, 255, 0)"
+  },
+  visible: {
+    pathLength: 1,
+    fill: "rgba(255, 255, 255, 1)"
+  }
+}
 
 export default function About() {
   return (
-    <MDBCard style={{ maxWidth: '22rem' }}>
-      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-        <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/111.webp' fluid alt='...' />
-        <a>
-          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-        </a>
-      </MDBRipple>
-      <MDBCardBody>
-        <MDBCardTitle>Card title</MDBCardTitle>
-        <MDBCardText>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </MDBCardText>
-        <MDBBtn href='#'>Button</MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
+    <>
+      <MDBCard style={{ maxWidth: '22rem' }}>
+        <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+          <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/111.webp' fluid alt='...' />
+          <a>
+            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+          </a>
+        </MDBRipple>
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </MDBCardText>
+          <MDBBtn href='#'>Button</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+      <div className="container">
+       <motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        className="item"
+        >
+        <motion.path
+          d="M0 100V0l50 50 50-50v100L75 75l-25 25-25-25z"
+          variants={icon}
+          initial="hidden"
+          animate="visible"
+          transition={{
+            default: { duration: 2, ease: "easeInOut" },
+            fill: { duration: 2, ease: [1, 0, 0.8, 1] }
+          }}
+        />
+        </motion.svg>
+      </div> 
+    </>
   );
 }
