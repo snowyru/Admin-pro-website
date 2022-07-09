a309821a939f8abb588817bf51cc87461e0d449c
snowyru <97807830+snowyru@users.noreply.github.com>
2022-07-09 14:23:51 +0200

    added logo to start

--- adminpro/src/APPages/Start.jsx
+++ adminpro/src/APPages/Start.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Start() {
   return (
   <>
-  <div style={{"backgroundColor":"#3c3c3c", "height":"50vw" }}>
-  <img src="https://i2.lensdump.com/i/tp3vIb.png" alt="tp3vIb.png" border="0" width="100%"/>
+  <div style={{"backgroundColor":"#3c3c3c"}}>
+  <img style={{"margin":"auto"}} src="https://i2.lensdump.com/i/tp3vIb.png" alt="tp3vIb.png" border="0" width="100%"/>
   </div>
   </>
   )
