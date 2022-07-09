040df1cf3ae5be6bfea44dde9d56d4e30562a127
snowyru <97807830+snowyru@users.noreply.github.com>
2022-07-09 11:20:19 +0200

    clean slate

--- adminpro/src/index.js
+++ adminpro/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
