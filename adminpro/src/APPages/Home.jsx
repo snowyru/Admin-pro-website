855e3ae26be23c3ab09a17fe76422b2e2a86e438
snowyru <97807830+snowyru@users.noreply.github.com>
2022-08-01 17:07:54 +0200

    added slogan with filecab img

--- adminpro/src/APPages/Home.jsx
+++ adminpro/src/APPages/Home.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import {motion} from 'framer-motion';
-import Typer from '../APAssets/Typer.jpg';
-import RedFiles from '../APAssets/RedFiles.jpg';
 import FileDrawer from '../APAssets/FileDrawer.jpg';
-import { useIsSmall } from '../FramerHook';
 
 export default function Home() {
 
   const transition = { duration: 1.4, ease:[0.6, 0.01, -0.05, 0.9]};
-  const isSmall = useIsSmall();
-  const variants = isSmall ?
-  {
-    animate:{
+  const text = { 
+    hidden:{
+      y:1000,
+      opactiy:0,
+    },
+    unhidden:{
       y:0,
-      width: "100%",
-      transition: {transition: { delay: 0.2, ...transition }}
+      opacity:1,
     }
   }
-  :
-  {
-    animate:{
-      y:0,
-      width: "100%",
-      transition: { delay: 0.2, ...transition }
+  const img={ 
+    hidden:{
+      opacity:0.1
+    },
+    unhidden:{
+      opacity:0.6,
+      transition:{duration:1.4, ease:[0.6, 0.01, -0.05, 0.9]},
     }
   }
   
@@ -31,7 +30,6 @@ export default function Home() {
   <>
 
   <motion.img 
-    variants={variants}
     animate={{ y: 0, width: "70%", transition: { delay: 0.2, ...transition } }}
     initial={{ width:"100%", y: "-15%" }}
     style={{}}
@@ -40,15 +38,15 @@ export default function Home() {
     alt="tp3vIb.png" border="0"/>
 
     <motion.div>
-      <motion.img src={FileDrawer} className="BGimg" />
-      <motion.h2 className="h2" > Be at the top of your game. Hire a virtual assistant! </motion.h2>
+      <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
+        viewport={{once:false, amount:0.6}}
+        src={FileDrawer} className="BGimg" />
+      <motion.div variants={text} initial={"hidden"} animate={"unhidden"}
+        transition={{staggerChildren:1}}>
+        <motion.h2 variants={text} transition={transition} className="lead"> Be at the top of your game.<br/> Hire a virtual assistant! </motion.h2>
+        <motion.p variants={text} transition={transition} className="sub"> At Admin Professional we provide specialised office administrative support to business owners’- professional entities to achieve their administrative goals. </motion.p>
+      </motion.div>
     </motion.div>
-
-  <motion.img src={RedFiles} className="leftSVG" initial={{ opacity: 0, x: -800 }}
-    animate={{ x: 0, opacity: 1, transition: { duration: 1, ...transition } }} />
-
-  <motion.img src={Typer} className="rightSVG" initial={{ opacity: 0, x: 800 }}
-    animate={{ x: 0, opacity: 1, transition: { duration: 1, ...transition } }} />
 
   </>
   );
