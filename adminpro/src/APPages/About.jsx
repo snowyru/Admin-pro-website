96fd8b856c58e960455c83f324a728107aa0ab60
snowyru <97807830+snowyru@users.noreply.github.com>
2022-08-09 16:12:07 +0200

    added client sayings

--- adminpro/src/APPages/About.jsx
+++ adminpro/src/APPages/About.jsx
@@ -10,6 +10,9 @@ import Avatar from '@mui/material/Avatar';
 import Backdrop from '@mui/material/Backdrop';
 import Button from '@mui/material/Button';
 import AccountBoxRoundedIcon from '@mui/icons-material/AccountBoxRounded';
+import FWBN from '../APAssets/FWBN.png';
+import ActionCoach from '../APAssets/ActionCoach.jpeg';
+import SASLAW from '../APAssets/SASLAW.png';
 // import FatimaLI from '../APAssets/FatimaLI.jpg';
 
 const icon = {
@@ -25,7 +28,7 @@ const icon = {
 
 const transition = { duration: 1.4, ease:[0.6, 0.01, -0.05, 0.9]};
 
-const REnter = { 
+const REnter = {
   hidden:{
     opacity: 0,
     x:300,
@@ -36,20 +39,59 @@ const REnter = {
   }
 };
 
-const RExit = { 
+const RExit = {
   opacity : 0,
   x : 1000,
   transition:{duration: 1.4, ease:[0.6, 0.01, -0.05, 0.9]},
 };
 
-const fatimaInfo = () => {
-  console.log("Hi")
-}
-const nuraanInfo = () => {
-  console.log("Bi")
+const info = {
+  hiddenL : {
+    opacity : 0,
+    x:-250,
+    scale : 0.3,
+    rotate: 90,
+  },
+  hiddenR : {
+    opacity : 0,
+    x:250,
+    scale : 0.3,
+    rotate: -90
+  },
+  unhidden : {
+    opacity : 1,
+    x:0,
+    scale : 1,
+    rotate:0,
+    transition:{transition}
+  },
+};
+const img={ 
+  hidden:{
+    opacity:0.5
+  },
+  unhidden:{
+    opacity:1,
+    borderRadius:[9,9,9,14,9,9],
+    transition:{repeat:Infinity, duration:1.4, ease:[0.6, 0.01, -0.05, 0.9]},
+  }
 }
 
 export default function About() {
+
+  // the team  
+  const [fatima, setfatima] = React.useState(false);
+  const [nuraan, setnuraan] = React.useState(false);
+
+  const fatimaInfo = () => {
+    setfatima(!fatima);
+    console.log("Hi")
+  }
+  const nuraanInfo = () => {
+    setnuraan(!nuraan);
+    console.log("bi")
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -81,13 +123,13 @@ export default function About() {
                   animate={"unhidden"}
                   transition={{staggerChildren:0.6}}>
                     <motion.h2 variants={REnter} transition={transition} className="leadSmall"> The story of Admin Professional </motion.h2>
-                    <motion.p variants={REnter} transition={transition} className="subSmall" > 
-                      Admin Professional opened its virtual doors in the year 2010 when virtual administration was not very common on South African shores. With the impact of the pandemic and with so many businesses resorting to “working from home” Admin Professional already had the tools and structures in place to flip the pandemic up-side-down! 
+                    <motion.p variants={REnter} transition={transition} className="subSmall" >
+                      Admin Professional opened its virtual doors in the year 2010 when virtual administration was not very common on South African shores. With the impact of the pandemic and with so many businesses resorting to “working from home” Admin Professional already had the tools and structures in place to flip the pandemic up-side-down!
                       <br/><br/>
                       We have positioned our administrative offering to assist small business owners, corporate entities and NGO’s with professional virtual administrative assistance where they do not require the services of full-time staff.
                     </motion.p>
                   </motion.div>
-                </Grid>              
+                </Grid>
               </Grid>
             </Box>
 
@@ -101,7 +143,7 @@ export default function About() {
                     animate={"unhidden"}
                     transition={{staggerChildren:0.6}}>
                     <motion.h2 variants={REnter} transition={transition} className="leadSmall"> Training & Development </motion.h2>
-                      <motion.p variants={REnter} transition={transition} className="subSmall" > 
+                      <motion.p variants={REnter} transition={transition} className="subSmall" >
                       Ongoing staff training and development is a priority so that we know we are delivering the best.
                       Hiring one of our staff members means that you have an entire team and a range of administrative expertise at your disposal!
                       </motion.p>
@@ -115,31 +157,111 @@ export default function About() {
                     initial={{width:"1%", y:-350, opacity:0}}
                     src={RedFiles} className="" />
                   </motion.div>
-                </Grid>              
+                </Grid>
               </Grid>
             </Box>
-            
+
             <motion.div>
-              <motion.h1 className="leadBig" >Meet the team </motion.h1>
+              <motion.h1 className="leadBig" > Meet the team </motion.h1>
                 <Grid container spacing={2}>
 
                   <Grid item xs={12} md={6}>
                     <motion.h1 onClick={fatimaInfo} className="leadName" > Fatima Rustin </motion.h1>
                     <Avatar onClick={fatimaInfo} alt="Fatima Rustin / Solomon" src="" sx= {{minWidth: "auto", minHeight: "auto"}}/>
                     <motion.h2 onClick={fatimaInfo} className="subSmall" > ~Founder & Prinical Administrator~ </motion.h2>
-                    <Button endIcon={<AccountBoxRoundedIcon/>} color="secondary" size="large" variant="contained" >More Info?</Button>
+                    <Box textAlign="center" >
+                      <Button style={{textTransform: 'lowercase'}} className="btnCenter" startIcon={<AccountBoxRoundedIcon/>} color="secondary" size="large" variant="contained" onClick={fatimaInfo} >More Info?</Button>
+                    </Box>
+                    <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                      open={fatima} onClick={fatimaInfo}>
+
+                      <motion.div variants={info} initial={"hiddenL"} animate={fatima ? "unhidden" : "hiddenL"} className="infoScreen">
+                        {fatima === true && 
+                          <div className="container">
+                            <h1 className="infoh1" >Fatima Rustin</h1>
+                            <p className="infop" >
+                            The owner of the business and holds the position of Principal Administrator. She has evidenced a lifelong commitment to education.  With a BA Business Degree in Human Ecology, a Diploma in Human Resource Management and Training and a Diploma in Bookkeeping, and completed and demonstrated an entrepreneurial apprenticeship with ActionCOACH Business Coaching.  
+
+                            Fatima has worked as an Administrative Team Leader, Human Resource Administrator and Executive Assistant to an executive entrepreneurial team, established and coordinated virtual teams and business entities and assisted professional bodies to coordinate national seminars and conferences and as a result of the pandemic
+
+                            coordinated virtual meetings and webinars. Fatima’s experience spans 23years, there is nothing about the challenges and administrative support requirements of big and small enterprises alike that she has not experienced and mastered firsthand. 
+
+                            Fatima is a proud mom of two teenagers and a busy seven-year-old boy.
+                            </p>
+                          </div>
+                        }  
+                      </motion.div>
+
+                    </Backdrop>
                   </Grid>
 
                   <Grid item xs={12} md={6}>
                     <motion.h1 onClick={nuraanInfo} className="leadName" > Nuraan Jattiem </motion.h1>
                     <Avatar onClick={nuraanInfo} alt="Nuraan Jattiem" src="" sx= {{minWidth: "auto", minHeight: "auto"}}/>
                     <motion.h2 onClick={nuraanInfo} className="subSmall" > ~Office Co-ordinator & Virtual Administrator~ </motion.h2>
-                    
+                    <Box textAlign="center" >
+                      <Button style={{textTransform: 'lowercase'}} className="btnCenter" startIcon={<AccountBoxRoundedIcon/>} color="secondary" size="large" variant="contained" onClick={nuraanInfo}>More Info?</Button>
+                    </Box>
+                    <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                      open={nuraan} onClick={nuraanInfo}>
+
+                      <motion.div variants={info} initial={"hiddenR"} animate={nuraan ? "unhidden" : "hiddenR"} className="infoScreen">
+                        {nuraan === true && 
+                          <div className="container">
+                            <h1 className="infoh1" >Nuraan Jattiem</h1>
+                            <p className="infop" >
+                            Nuraan Jattiem joins our team as the Office Co-ordinator and Virtual Administrator. Norman’s background of administration within the legal and imports environment demonstrates years of front office administration experience which includes accounts administration, document processing and report writing and data capturing. Nuraan coordinates the schedules of our busy virtual administrative office and is assigned to a long-standing client for complete front office administrative support. She joined the team last year but has already proven competencies in key areas required for excellence in virtual administration.
+                            Nuraan has also co-launched a small business called “Mini Chefs” and is a mom to two amazing daughters.
+                            </p>
+                          </div> 
+                        }  
+                      </motion.div>
+
+                    </Backdrop>
                   </Grid>
 
                 </Grid>
             </motion.div>
 
+            <motion.div>
+              <motion.h2 className="leadBig" > What our clients are saying </motion.h2>
+
+                <motion.div className="leadBig" ><motion.div className="imgSpace hideScroll" >
+                  <motion.img variants={img} animate={"unhidden"}
+                  src={FWBN} className="clientImg" />
+                </motion.div>
+                  <motion.p style={{"color": "white"}} className="sub" >
+                    “Admin Professional always imparts vision and determination. They are always a pleasure to deal with as they are professional, knowledgeable and a great asset to our organisation. I do not hesitate to recommend her.” <motion.b> – Fine Women Business Network</motion.b>
+                  </motion.p>
+                </motion.div>
+
+                <motion.div className="leadBig"><motion.div className="imgSpace hideScroll" >
+                  <motion.img variants={img} animate={"unhidden"}
+                  src={ActionCoach} className="clientImg topPad" />
+                  </motion.div>
+                  <motion.p style={{"color": "white"}} className="sub" >
+                    “As an entrepreneur who doesn’t like paperwork & a business owner that is not yet big enough for a full-time PA, well done Admin Professional for the solution of a timeshare PA. There is no more admin chaos and no pressure at having to fill a half-day every day, and affordable - I can match my staff bill to my rate of growth. Brilliant.” <br/> <motion.b> – Kathi Clarke Industrial Psychologist and Business Coach – Action Coach</motion.b>
+                  </motion.p>
+                </motion.div>  
+                
+                <motion.div className="leadBig"><motion.div className="imgSpace hideScroll" >
+                  <motion.img variants={img} animate={"unhidden"}
+                  src={SASLAW} className="clientImg topPad" />
+                </motion.div>
+                  <motion.p style={{"color": "white"}} className="sub" >                   
+                  “I’ve had the privilege of working with Fatima for 7 years in my role as the General Manager of SASLAW. She is very organised and is a great planner and delivers a structured approach to projects.   She is creative and clever with new ideas and concepts suitable for her clients,
+                  and is an expert in Client Relationship management. Fatima takes the time to understand the needs and requirements of her clients and stakeholders and delivers accordingly. I wish her all the very best for the future.”  <br/> <motion.b> – Clare Fincham – General Manager - SASLAW</motion.b>
+                  </motion.p>
+                </motion.div>  
+
+                <motion.div>
+                  <motion.h1 style={{"borderBottom":"0px"}} className="leadBig" >Some of our clients over past the years include</motion.h1>
+
+                        
+                  
+                </motion.div>
+
+            </motion.div>
 
           </motion.div>
         </motion.div>
@@ -164,7 +286,7 @@ export default function About() {
           }}
         />
         </motion.svg>
-      </div> 
+      </div>
     </>
   );
 }
