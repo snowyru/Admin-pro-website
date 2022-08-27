a10a40d467917c9b54e1915abb440c9b5976dd6d
snowyru <97807830+snowyru@users.noreply.github.com>
2022-08-27 14:15:36 +0200

    hidescroll home class

--- src/APPages/Home.jsx
+++ src/APPages/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
       <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
         viewport={{once:false, amount:0.35}}
         src={FileDrawer} className="BGimghome" />
-      <motion.div style={{hideScroll:"hidden"}} variants={text} initial={"hidden"} animate={"unhidden"}
+      <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
         transition={{staggerChildren:1}}>
         <motion.h2 variants={text} transition={transition} className="lead"> Be at the top of your game.<br/> Hire a virtual assistant! </motion.h2>
         <motion.p variants={text} transition={transition} className="sub"> At Admin Professional we provide specialised office administrative support to business owners’- professional entities to achieve their administrative goals. </motion.p>
@@ -63,7 +63,7 @@ export default function Home() {
      <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
         viewport={{once:false, amount:0.6}}
         src={HandShaker} className="BGimghome imgSpace" />
-      <motion.div style={{hideScroll:"hidden"}} variants={text} initial={"hidden"} animate={"unhidden"}
+      <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
         transition={{staggerChildren:1}}>
         <motion.h2 variants={text} transition={transition} className="lead"> How can we help you…? </motion.h2>
         <motion.p variants={text} transition={transition} className="sub"> Services offered range from:
@@ -80,7 +80,7 @@ export default function Home() {
      <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
         viewport={{once:false, amount:0.6}}
         src={Typer} className="BGimghome imgSpace" />
-      <motion.div style={{hideScroll:"hidden"}} variants={text} initial={"hidden"} animate={"unhidden"}
+      <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
         transition={{staggerChildren:1}}>
         <motion.h2 variants={text} transition={transition} className="lead"> We are recommended </motion.h2>
         <motion.p variants={text} transition={transition} className="sub"> All our marketing is communicated via word-of-mouth. We operate as a small skilled team and bring value to business owners who understands the value of a skilled team. We understand that client information is highly confidential, and we treat each client with the highest integrity. We do not have social media pages but follow many professional and business individuals and groups to keep up to date with topical matters and trends. 
@@ -92,7 +92,7 @@ export default function Home() {
      <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
         viewport={{once:false, amount:0.6}}
         src={HandPlant} className="BGimghome imgSpace" />
-      <motion.div style={{hideScroll:"hidden"}} variants={text} initial={"hidden"} animate={"unhidden"}
+      <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
         transition={{staggerChildren:1}}>
         <motion.h2 variants={text} transition={transition} className="lead"> Our passion for giving back </motion.h2>
         <motion.p variants={text} transition={transition} className="sub"> The mission of Admin Professional is not only to provide professional administrative support in line with best practices but to do this through the sustainable empowerment and development of communities. Our projects to date include stationary drives which are distributed to the underprivileged and noted we have sponsored over 700 hours of administrative assistance to the value of R 154 000,00. 
