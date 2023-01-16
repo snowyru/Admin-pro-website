11805f64c488483f6da2e03f5e903c29726afe80
snowyru <97807830+snowyru@users.noreply.github.com>
2023-01-16 19:11:01 +0200

    added public resources

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,46 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
+import {motion} from 'framer-motion';
+import FileDrawer from '../Public/APAssets/FileDrawer.jpg';
+import HandShaker from '../Public/APAssets/HandShaker.jpg';
+import Typer from '../Public/APAssets/Typer.jpg';
+import HandPlant from '../Public/APAssets/HandPlant.jpg';
+import LOGO from '../Public/APAssets/LOGO.jpg';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const transition = { duration: 1.4, ease:[0.6, 0.01, -0.05, 0.9]};
+  const text = { 
+    hidden:{
+      y:1000,
+      opactiy:0,
+    },
+    unhidden:{
+      y:0,
+      opacity:1,
+    }
+  }
+  const img={ 
+    hidden:{
+      scale:0.85,
+      opacity:0.1
+    },
+    unhidden:{
+      scale:1,
+      opacity:0.55,
+      transition:{duration:1.4, ease:[0.6, 0.01, -0.05, 0.9]},
+    }
+  }
+  
+  const exit = { 
+    opacity : 0,
+    x : -400,
+    transition:{duration: 1.4, ease:[0.6, 0.01, -0.05, 0.9]},
+   }
+
   return (
     <>
       <Head>
@@ -14,111 +51,74 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            yoooo whats up everybody hows yalll doing
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      
+        <motion.div style={{width:"100px"}} exit={{width:"100%", y:"-15%", transition: { delay: 0.2, ...transition } }}
+        animate={{ y: 0, width: "70%", transition: { delay: 0.2, ...transition } }}
+        initial={{ width:"100%", y: "-15%" }}
+        className="TransLogo" >
+            <Image src={LOGO} alt="logo" style={{objectFit:"contain"}} />
+        </motion.div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+      {/* <motion.img exit={{width:"100%", y:"-15%", transition: { delay: 0.2, ...transition } }}
+        animate={{ y: 0, width: "70%", transition: { delay: 0.2, ...transition } }}
+        initial={{ width:"100%", y: "-15%" }}
+        style={{}}
+        className="TransLogo"
+        src={LOGO} loading="lazy"
+        alt="tp3vIb.png" border="0"/>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+        <motion.div exit={exit} className="hideScroll flexer Txt">
+          <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
+            viewport={{once:false, amount:0.35}} loading="lazy"
+            src={FileDrawer} className="BGimghome" />
+          <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
+            transition={{staggerChildren:1}}>
+            <motion.h2 variants={text} transition={transition} className="lead"> Be at the top of your game.<br/> Hire a virtual assistant! </motion.h2>
+            <motion.p variants={text} transition={transition} className="sub"> At Admin Professional we provide specialised office administrative support to business owners’- professional entities to achieve their administrative goals. </motion.p>
+          </motion.div>
+        </motion.div>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+        <motion.div exit={exit} className="flexer Txt" >
+        <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
+            viewport={{once:false, amount:0.6}} loading="lazy"
+            src={HandShaker} className="BGimghome imgSpace" />
+          <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
+            transition={{staggerChildren:1}}>
+            <motion.h2 variants={text} transition={transition} className="lead"> How can we help you…? </motion.h2>
+            <motion.p variants={text} transition={transition} className="sub"> Services offered range from:
+            General front office administration,
+            Co-ordination of seminars and registrations, general HR Administration, 
+            Small project team administration logistics.
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+            For further information e-mail us at <a href='mailto:info@adminprofessional.net' target='blank' >info@adminprofessional.net</a> 
+            </motion.p>
+          </motion.div>
+        </motion.div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <motion.div exit={exit} className="imgContainer flexer Txt">
+        <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
+            viewport={{once:false, amount:0.6}} loading="lazy"
+            src={Typer} className="BGimghome imgSpace" />
+          <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
+            transition={{staggerChildren:1}}>
+            <motion.h2 variants={text} transition={transition} className="lead"> We are recommended </motion.h2>
+            <motion.p variants={text} transition={transition} className="sub"> All our marketing is communicated via word-of-mouth. We operate as a small skilled team and bring value to business owners who understands the value of a skilled team. We understand that client information is highly confidential, and we treat each client with the highest integrity. We do not have social media pages but follow many professional and business individuals and groups to keep up to date with topical matters and trends. 
+            </motion.p>
+          </motion.div>
+        </motion.div>
+
+        <motion.div exit={exit} className="imgContainer flexer Txt">
+        <motion.img variants={img} initial={"hidden"} whileInView={"unhidden"}
+            viewport={{once:false, amount:0.6}} loading="lazy"
+            src={HandPlant} className="BGimghome imgSpace" />
+          <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
+            transition={{staggerChildren:1}}>
+            <motion.h2 variants={text} transition={transition} className="lead"> Our passion for giving back </motion.h2>
+            <motion.p variants={text} transition={transition} className="sub"> The mission of Admin Professional is not only to provide professional administrative support in line with best practices but to do this through the sustainable empowerment and development of communities. Our projects to date include stationary drives which are distributed to the underprivileged and noted we have sponsored over 700 hours of administrative assistance to the value of R 154 000,00. 
+            </motion.p>
+          </motion.div>
+        </motion.div> */}
+
     </>
   )
 }
