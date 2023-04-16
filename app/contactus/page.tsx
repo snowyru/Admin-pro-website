1f59c9b89782bf3f9811b319288cc9873d5e9c39
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2023-04-16 16:36:13 +0200

    reset nextjs13

--- app/contactus/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contactus() {
-    return (
-        <div>
-            Contact us page
-        </div>
-    );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,124 @@
-'use client'
-import React from 'react';
-import Image from 'next/image';
-import {motion} from 'framer-motion';
-import FileDrawer from '../public/FileDrawer.jpg';
-import HandShaker from '../public/HandShaker.jpg';
-import Typer from '../public/Typer.jpg';
-import HandPlant from '../public/HandPlant.jpg';
-import LOGO from '../public/LOGO.jpg';
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
 
-const Home = () => {
+const inter = Inter({ subsets: ['latin'] })
 
-  const transition = { duration: 1.4, ease:[0.6, 0.01, -0.05, 0.9]};
-  const text = { 
-    hidden:{
-      y:1000,
-      opactiy:0,
-    },
-    unhidden:{
-      y:0,
-      opacity:1,
-    }
-  }
-  const img={ 
-    hidden:{
-      scale:0.85,
-      opacity:0.1
-    },
-    unhidden:{
-      scale:1,
-      opacity:0.55,
-      transition:{duration:1.4, ease:[0.6, 0.01, -0.05, 0.9]},
-    }
-  }
-  
-  const exit = { 
-    opacity : 0,
-    x : -400,
-    transition:{duration: 1.4, ease:[0.6, 0.01, -0.05, 0.9]},
-   }
-
+export default function Home() {
   return (
-  <>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
 
-    <motion.div exit={{width:"100%", y:"-15%", transition: { delay: 0.2, ...transition } }}
-        animate={{ y: 0, width: "70%", transition: { delay: 0.2, ...transition } }}
-        initial={{ width:"100%", y: "-15%" }}
-        style={{}}
-        className="TransLogo">
-        <Image src={LOGO} loading="lazy" alt="tp3vIb.png" style={{border: 0}}/>
-    </motion.div>
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
 
-    <motion.div exit={exit} className="hideScroll flexer Txt">
-        <motion.div variants={img} initial={"hidden"} whileInView={"unhidden"}
-            viewport={{once:false, amount:0.35}}
-            className="BGimghome">
-            <Image src={FileDrawer} alt="" style={{border: 0}}/>
-        </motion.div>
-        <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
-            transition={{staggerChildren:1}}>
-            <motion.h2 variants={text} transition={transition} className="lead"> Be at the top of your game.<br/> Hire a virtual assistant! </motion.h2>
-            <motion.p variants={text} transition={transition} className="sub"> At Admin Professional we provide specialised office administrative support to business owners’- professional entities to achieve their administrative goals. </motion.p>
-        </motion.div>
-    </motion.div>
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
 
-    <motion.div exit={exit} className="flexer Txt" >
-        <motion.div variants={img} initial={"hidden"} whileInView={"unhidden"}
-            viewport={{once:false, amount:0.6}}
-            className="BGimghome imgSpace">
-            <Image src={HandShaker} alt="" style={{border: 0}}/>
-        </motion.div>
-        <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
-            transition={{staggerChildren:1}}>
-            <motion.h2 variants={text} transition={transition} className="lead"> How can we help you…? </motion.h2>
-            <motion.p variants={text} transition={transition} className="sub"> Services offered range from:
-            General front office administration,
-            Co-ordination of seminars and registrations, general HR Administration, 
-            Small project team administration logistics.
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Learn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
 
-            For further information e-mail us at <a href='mailto:info@adminprofessional.net' target='blank' >info@adminprofessional.net</a> 
-            </motion.p>
-        </motion.div>
-    </motion.div>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Explore the Next.js 13 playground.
+          </p>
+        </a>
 
-    <motion.div exit={exit} className="imgContainer flexer Txt">
-        <motion.div variants={img} initial={"hidden"} whileInView={"unhidden"}
-            viewport={{once:false, amount:0.6}}
-            className="BGimghome imgSpace">
-            <Image src={Typer} alt="" style={{border: 0}}/>
-        </motion.div>
-        <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
-            transition={{staggerChildren:1}}>
-            <motion.h2 variants={text} transition={transition} className="lead"> We are recommended </motion.h2>
-            <motion.p variants={text} transition={transition} className="sub"> All our marketing is communicated via word-of-mouth. We operate as a small skilled team and bring value to business owners who understands the value of a skilled team. We understand that client information is highly confidential, and we treat each client with the highest integrity. We do not have social media pages but follow many professional and business individuals and groups to keep up to date with topical matters and trends. 
-            </motion.p>
-        </motion.div>
-    </motion.div>
-
-    <motion.div exit={exit} className="imgContainer flexer Txt">
-      <motion.div variants={img} initial={"hidden"} whileInView={"unhidden"}
-          viewport={{once:false, amount:0.6}}
-          className="BGimghome imgSpace">
-          <Image src={HandPlant} alt="" style={{border: 0}}/>
-      </motion.div>
-        <motion.div className="hideScroll" variants={text} initial={"hidden"} animate={"unhidden"}
-            transition={{staggerChildren:1}}>
-            <motion.h2 variants={text} transition={transition} className="lead"> Our passion for giving back </motion.h2>
-            <motion.p variants={text} transition={transition} className="sub"> The mission of Admin Professional is not only to provide professional administrative support in line with best practices but to do this through the sustainable empowerment and development of communities. Our projects to date include stationary drives which are distributed to the underprivileged and noted we have sponsored over 700 hours of administrative assistance to the value of R 154 000,00. 
-            </motion.p>
-        </motion.div>
-    </motion.div>
-  </>
-  );
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p
+            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          >
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
 }
-
-export default Home
